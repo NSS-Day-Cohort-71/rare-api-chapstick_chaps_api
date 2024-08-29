342b214fe270f192539899d278b4ac57ec3e1ab8
Nicholas Winkler <nhwinkler@gmail.com>
2024-08-29 14:58:24 -0500

    Implemented functionality for creating a new cateory.

--- views/__init__.py
+++ views/__init__.py
@@ -1,4 +1,4 @@
 from .user import login_user, create_user
-from .category import get_categories
+from .category import get_categories, create_category
 from .tag import get_tags, create_tag
 from .post import create_post
