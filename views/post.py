fad37989baa0191779c0ac65ce906bafe6b47e9b
Caleb Ford <caleb.ford.sto@gmail.com>
2024-08-30 15:55:32 -0500

    functioning my posts view, needs more css

--- views/post.py
+++ views/post.py
@@ -26,6 +26,7 @@ def create_post(post):
 
         return json.dumps({"post_id": id})
 
+
 def get_posts():
     with sqlite3.connect("./db.sqlite3") as conn:
         conn.row_factory = sqlite3.Row
@@ -55,33 +56,95 @@ def get_posts():
         query_results = db_cursor.fetchall()
         posts = []
         for row in query_results:
-            category = {
-                "id": row['categoryId'],
-                "label": row['categoryLabel'],
-            } if row['categoryId'] else {
-                "id": None,
-                "label": "no category available",
-            }
-            user = {
-                "id": row['userId'],
-                "username": row['username']
-            }
+            category = (
+                {
+                    "id": row["categoryId"],
+                    "label": row["categoryLabel"],
+                }
+                if row["categoryId"]
+                else {
+                    "id": None,
+                    "label": "no category available",
+                }
+            )
+            user = {"id": row["userId"], "username": row["username"]}
             post = {
-                "id": row['id'],
-                "user_id": row['userId'],
-                "category_id": row['categoryId'],
-                "title": row['title'],
-                "publication_date": row['publication_date'],
+                "id": row["id"],
+                "user_id": row["userId"],
+                "category_id": row["categoryId"],
+                "title": row["title"],
+                "publication_date": row["publication_date"],
                 "image_url": row["image_url"],
                 "content": row["content"],
                 "approved": row["approved"],
                 "category": category,
-                "user": user
+                "user": user,
             }
             posts.append(post)
 
         serialized_posts = json.dumps(posts)
     return serialized_posts
+
+
+def get_posts_by_user_id(id):
+    with sqlite3.connect("./db.sqlite3") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        db_cursor.execute(
+            """
+                SELECT
+                    p.id,
+                    p.user_id,
+                    u.id as userId,
+                    u.username as username,
+                    p.category_id,
+                    c.id as categoryId,
+                    c.label as categoryLabel,
+                    p.title,
+                    p.publication_date,
+                    p.image_url,
+                    p.content,
+                    p.approved
+            FROM Posts p
+            LEFT JOIN Categories c ON p.category_id = c.id
+            LEFT JOIN Users u ON p.user_id = u.id
+            WHERE p.user_id = ?
+            """,
+            (id,),
+        )
+        query_results = db_cursor.fetchall()
+        posts = []
+        for row in query_results:
+            category = (
+                {
+                    "id": row["categoryId"],
+                    "label": row["categoryLabel"],
+                }
+                if row["categoryId"]
+                else {
+                    "id": None,
+                    "label": "no category available",
+                }
+            )
+            user = {"id": row["userId"], "username": row["username"]}
+            post = {
+                "id": row["id"],
+                "user_id": row["userId"],
+                "user": user,
+                "category_id": row["categoryId"],
+                "category": category,
+                "title": row["title"],
+                "publication_date": row["publication_date"],
+                "image_url": row["image_url"],
+                "content": row["content"],
+                "approved": row["approved"],
+            }
+            posts.append(post)
+
+        serialized_posts = json.dumps(posts)
+    return serialized_posts
+
 
 def get_postById(pk):
     with sqlite3.connect("./db.sqlite3") as conn:
@@ -113,28 +176,29 @@ def get_postById(pk):
 
         row = db_cursor.fetchone()
         if row:
-            category = {
-                "id": row['categoryId'],
-                "label": row['categoryLabel'],
-            } if row['categoryId'] else {
-                "id": None,
-                "label": "no category available",
-            }
-            user = {
-                "id": row['userId'],
-                "username": row['username']
-            }
+            category = (
+                {
+                    "id": row["categoryId"],
+                    "label": row["categoryLabel"],
+                }
+                if row["categoryId"]
+                else {
+                    "id": None,
+                    "label": "no category available",
+                }
+            )
+            user = {"id": row["userId"], "username": row["username"]}
             post = {
-                "id": row['id'],
-                "user_id": row['userId'],
-                "category_id": row['categoryId'],
-                "title": row['title'],
-                "publication_date": row['publication_date'],
+                "id": row["id"],
+                "user_id": row["userId"],
+                "category_id": row["categoryId"],
+                "title": row["title"],
+                "publication_date": row["publication_date"],
                 "image_url": row["image_url"],
                 "content": row["content"],
                 "approved": row["approved"],
                 "category": category,
-                "user": user
+                "user": user,
             }
             serialized_post = json.dumps(post)
             return serialized_post
